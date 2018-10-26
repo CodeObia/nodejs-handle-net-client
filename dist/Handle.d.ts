6d8fc9004431f91448e39cab01ebb77ef1c48e49
moayadnajd <moayad.najd@gmail.com>
2018-10-26 16:06:43 +0200

    update

--- dist/Handle.d.ts
+++ dist/Handle.d.ts
@@ -1,5 +1,5 @@
 export declare class Handle {
-    constructor(ServerURL: string, userPrefix: string, keyPath: string, log: boolean = false)
+    constructor(ServerURL: string, userPrefix: string, keyPath: string, log: boolean)
     deleteHandle(id: string): Promise<any>
     newHandle(url: string): Promise<any>
     listHandles(): Promise<any>
